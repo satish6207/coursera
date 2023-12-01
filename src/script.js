a905e9f9af396dc7788324e87969be41387a2043
satish6207 <121442641+satish6207@users.noreply.github.com>
2023-12-01 09:51:59 +0530

    responsive correction

--- src/script.js
+++ src/script.js
@@ -10,3 +10,32 @@ function filterCards() {
         card.style.display = selectedCheckbox.every(skill => skills.includes(skill)) ? 'block' : 'none';
     });
 }
+
+// carousel section
+
+let currentIndex = 0;
+const items = document.querySelectorAll('.carousel-item');
+const totalItems = items.length;
+
+function nextSlide() {
+  if (currentIndex < totalItems - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  updateCarousel();
+}
+
+function prevSlide() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = totalItems - 1;
+  }
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const newTransformValue = -currentIndex * 100 + '%';
+  document.querySelector('.carousel-inner').style.transform = 'translateX(' + newTransformValue + ')';
+}
